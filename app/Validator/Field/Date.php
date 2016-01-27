417a2fd7382f7427abfa6b32f5ae37800af1221b
tombazza <tombazza@gmail.com>
2016-01-27 15:02:09 +0000

    Fixed date checking validation

--- app/Validator/Field/Date.php
+++ app/Validator/Field/Date.php
@@ -5,15 +5,16 @@ use Register\Validator\Field;
 class Date extends Field {
 	
 	public function validate() {
-		// Check using a regular expression that the date matches dd/mm/yyyy
-		// Ideally improve this as at present it's not validating if the date is real!
+		// Check using a regular expression that the date pattern matches dd/mm/yyyy
 		$this->fieldContent = filter_input($this->inputMethod, $this->field, FILTER_VALIDATE_REGEXP, [
 			'options' => [
 				'regexp' => '/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/'
 			]
 		]);
 		
-		if(!$this->fieldContent) {
+		// Split the date up so that it can be validated as a real date
+		$dateParts = explode("/", $this->fieldContent);
+		if(!$this->fieldContent || !checkdate($dateParts[1], $dateParts[0], $dateParts[2])) {
 			$this->errors[] = 'Not a valid date';
 			return false;
 		}
